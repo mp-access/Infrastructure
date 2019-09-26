a29aaa1d4722856f3d2255d8a0e117ee313fd425
Alexander Hofmann <alexhofmann@gmail.com>
2019-09-26 23:29:41 +0200

    Fix permissions on dump

--- scripts/backup-postgres.sh
+++ scripts/backup-postgres.sh
@@ -1,18 +1,25 @@
+
 if [ "$#" -ne 2 ]; then
     echo "[Usage]: backup-postgres.sh <backup filename> <path to backup folder>"
     exit 1
 fi
 
-DUMP_FILE=$1
+DUMP_FILE="$1-`date +%Y-%m-%d"_"%H_%M_%S`.sql"
+FULL_PATH=$2/$DUMP_FILE
+TMP_FILE="/tmp/$DUMP_FILE"
 BACKUP_PARTITION=$2
-FULL_PATH=$2/$1
 
 # Read DB_USER env variable and password
 export $(cat .env | grep "^DB_USER=")
 export $(cat .env | grep "^DB_PASSWORD=")
 
 # Generate dump and put it into /tmp/dump.sql
-docker-compose exec postgres pg_dumpall -U $DB_USER --clean > "$FULL_PATH-`date +%Y-%m-%d"_"%H_%M_%S`.sql"
+docker-compose exec postgres pg_dumpall -U $DB_USER --clean -f $TMP_FILE
 
 # Set permissions to read outside of container
-#chown 1005:100 $FULL_PATH
+docker-compose exec postgres chmod ug+rx $TMP_FILE
+
+docker cp "$(docker-compose ps -q postgres)":$TMP_FILE $FULL_PATH
+
+# Remove backup inside container
+docker-compose exec postgres rm -rf $TMP_FILE
