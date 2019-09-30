272055fc1ca71f82db7d610470edf36ded1aff49
Alexander Hofmann <alexhofmann@gmail.com>
2019-09-30 23:03:45 +0200

    Add -T flag to compose commands
    Add timestamp to logging

--- scripts/backup-mongo.sh
+++ scripts/backup-mongo.sh
@@ -4,36 +4,36 @@ if [ "$#" -ne 2 ]; then
     exit 1
 fi
 
-echo "Running mongodb backup script"
+echo "$(date -u) Running mongodb backup script"
 
 DUMP_FILE="$1-`date +%Y-%m-%d"_"%H_%M_%S`"
 FULL_PATH=$2/$DUMP_FILE
 TMP_FILE="/tmp/$DUMP_FILE"
 BACKUP_PARTITION=$2
 
-echo "Will save backup to $FULL_PATH"
+echo "$(date -u) Will save backup to $FULL_PATH"
 
 # Read DB_USER env variable and password
 export $(cat .env | grep "^MONGO_DB_USER=")
 export $(cat .env | grep "^MONGO_DB_PASSWORD=")
 
-echo "Generating backup. Connecting as user $MONGO_DB_USER"
+echo "$(date -u) Generating backup. Connecting as user $MONGO_DB_USER"
 
 # Generate dump and put it into /tmp/dump.sql
-/usr/local/bin/docker-compose exec mongo mongodump -u $MONGO_DB_USER -p $MONGO_DB_PASSWORD --out $TMP_FILE
+/usr/local/bin/docker-compose exec -T mongo mongodump -u $MONGO_DB_USER -p $MONGO_DB_PASSWORD --out $TMP_FILE
 
-echo "Setting correct user permissions"
+echo "$(date -u) Setting correct user permissions"
 
 # Set permissions to read outside of container
-/usr/local/bin/docker-compose exec mongo chmod ug+rx $TMP_FILE
+/usr/local/bin/docker-compose exec -T mongo chmod ug+rx $TMP_FILE
 
-echo "Copy backup outside of container"
+echo "$(date -u) Copy backup outside of container"
 
 docker cp "$(/usr/local/bin/docker-compose ps -q mongo)":$TMP_FILE $FULL_PATH
 
-echo "Cleanup"
+echo "$(date -u) Cleanup"
 
 # Remove backup inside container
-/usr/local/bin/docker-compose exec mongo rm -rf $TMP_FILE
+/usr/local/bin/docker-compose exec -T mongo rm -rf $TMP_FILE
 
-echo "Done"
+echo "$(date -u) Done"
