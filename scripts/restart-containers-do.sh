c51da63587bd145fc6b209487dee56bba5e9d4a1
Alexander Hofmann <alexhofmann@gmail.com>
2019-10-16 19:40:56 +0200

    Change docker compose file

--- scripts/restart-containers-do.sh
+++ scripts/restart-containers-do.sh
@@ -2,4 +2,4 @@ docker pull hoal/frontend:dev
 docker pull hoal/course-service:dev
 
 docker-compose down
-docker-compose -f docker-compose-do.yml up -d
+docker-compose -f docker-compose-tag.yml up -d
