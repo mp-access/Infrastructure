29767d6f78c1eb4be297ff17cb32cded46206920
Alexander Hofmann <alexhofmann@gmail.com>
2019-11-17 15:16:52 +0100

    Add pull analytics back n frontend

--- scripts/restart-containers-do.sh
+++ scripts/restart-containers-do.sh
@@ -1,4 +1,6 @@
 docker pull hoal/frontend:dev
 docker pull hoal/course-service:dev
+docker pull hoal/analytics-backend:master
+docker pull hoal/analytics-frontend:master
 
 docker-compose -f docker-compose-tag.yml -f docker-compose.analytics.yml up -d
