2a135f11fcf4ff04b1dfeff50a9eab0a6608d195
Alexander Hofmann <alexhofmann@gmail.com>
2019-11-17 15:15:56 +0100

    Add analytics to restart-containers-do.sh

--- scripts/restart-containers-do.sh
+++ scripts/restart-containers-do.sh
@@ -1,4 +1,4 @@
 docker pull hoal/frontend:dev
 docker pull hoal/course-service:dev
 
-docker-compose -f docker-compose-tag.yml up -d
+docker-compose -f docker-compose-tag.yml -f docker-compose.analytics.yml up -d
