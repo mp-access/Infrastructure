d81727c725dd07ca7eed5271a2da25e594837516
Alexander Hofmann <alexhofmann@gmail.com>
2019-11-22 01:25:02 +0100

    Add release script

---
+++ scripts/release-containers.sh
@@ -0,0 +1,17 @@
+IMAGE=$1
+VERSION=$2
+
+echo "Pulling master container of $IMAGE and tagging as follows: 'latest', '$VERSION', '$VERSION-master'"
+read -p "Confirm [Y/n]" -n 1 -r
+echo    # (optional) move to a new line
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+    [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1 # handle exits from shell or function but don't exit interactive shell
+fi
+
+docker pull "$IMAGE:master"
+IMAGE_ID=$(docker images "$IMAGE:master" -q)
+echo "$IMAGE_ID"
+docker tag "$IMAGE_ID" "$IMAGE:latest"
+docker tag "$IMAGE_ID" "$IMAGE:$VERSION"
+docker tag "$IMAGE_ID" "$IMAGE:$VERSION-master"
