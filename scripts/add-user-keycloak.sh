e7a34df6b1781d8028b1e588189ba3a41a07a9ea
Alexander Hofmann <alexhofmann@gmail.com>
2020-01-31 23:26:43 +0100

    Fix script location and restart keycloak after adding user

--- scripts/add-user-keycloak.sh
+++ scripts/add-user-keycloak.sh
@@ -1,1 +1,3 @@
-docker exec $(docker-compose ps -q keycloak) keycloak/bin/add-user-keycloak.sh -u admin -p "123456"
+set -ex
+docker exec $(docker-compose ps -q keycloak) /opt/jboss/keycloak/bin/add-user-keycloak.sh -u admin -p "123456"
+docker-compose restart keycloak
